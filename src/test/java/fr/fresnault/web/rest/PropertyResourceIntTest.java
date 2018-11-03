7b314da333e4c8f4a5cfc3c0f4c864ec1f9e5a3b
François Esnault <francois.esnault53@gmail.com>
2018-11-03 11:35:30 +0100

    Generated with JHipster 5.3.1

---
+++ src/test/java/fr/fresnault/web/rest/PropertyResourceIntTest.java
@@ -0,0 +1,217 @@
+package fr.fresnault.web.rest;
+
+import fr.fresnault.LeboncoinListScrapperApp;
+
+import fr.fresnault.domain.Property;
+import fr.fresnault.repository.PropertyRepository;
+import fr.fresnault.web.rest.errors.ExceptionTranslator;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.List;
+
+
+import static fr.fresnault.web.rest.TestUtil.createFormattingConversionService;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.hasItem;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+/**
+ * Test class for the PropertyResource REST controller.
+ *
+ * @see PropertyResource
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = LeboncoinListScrapperApp.class)
+public class PropertyResourceIntTest {
+
+    @Autowired
+    private PropertyRepository propertyRepository;
+
+    @Autowired
+    private MappingJackson2HttpMessageConverter jacksonMessageConverter;
+
+    @Autowired
+    private PageableHandlerMethodArgumentResolver pageableArgumentResolver;
+
+    @Autowired
+    private ExceptionTranslator exceptionTranslator;
+
+    private MockMvc restPropertyMockMvc;
+
+    private Property property;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        final PropertyResource propertyResource = new PropertyResource(propertyRepository);
+        this.restPropertyMockMvc = MockMvcBuilders.standaloneSetup(propertyResource)
+            .setCustomArgumentResolvers(pageableArgumentResolver)
+            .setControllerAdvice(exceptionTranslator)
+            .setConversionService(createFormattingConversionService())
+            .setMessageConverters(jacksonMessageConverter).build();
+    }
+
+    /**
+     * Create an entity for this test.
+     *
+     * This is a static method, as tests for other entities might also need it,
+     * if they test an entity which requires the current entity.
+     */
+    public static Property createEntity() {
+        Property property = new Property();
+        return property;
+    }
+
+    @Before
+    public void initTest() {
+        propertyRepository.deleteAll();
+        property = createEntity();
+    }
+
+    @Test
+    public void createProperty() throws Exception {
+        int databaseSizeBeforeCreate = propertyRepository.findAll().size();
+
+        // Create the Property
+        restPropertyMockMvc.perform(post("/api/properties")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(property)))
+            .andExpect(status().isCreated());
+
+        // Validate the Property in the database
+        List<Property> propertyList = propertyRepository.findAll();
+        assertThat(propertyList).hasSize(databaseSizeBeforeCreate + 1);
+        Property testProperty = propertyList.get(propertyList.size() - 1);
+    }
+
+    @Test
+    public void createPropertyWithExistingId() throws Exception {
+        int databaseSizeBeforeCreate = propertyRepository.findAll().size();
+
+        // Create the Property with an existing ID
+        property.setId("existing_id");
+
+        // An entity with an existing ID cannot be created, so this API call must fail
+        restPropertyMockMvc.perform(post("/api/properties")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(property)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Property in the database
+        List<Property> propertyList = propertyRepository.findAll();
+        assertThat(propertyList).hasSize(databaseSizeBeforeCreate);
+    }
+
+    @Test
+    public void getAllProperties() throws Exception {
+        // Initialize the database
+        propertyRepository.save(property);
+
+        // Get all the propertyList
+        restPropertyMockMvc.perform(get("/api/properties?sort=id,desc"))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(property.getId())));
+    }
+    
+    @Test
+    public void getProperty() throws Exception {
+        // Initialize the database
+        propertyRepository.save(property);
+
+        // Get the property
+        restPropertyMockMvc.perform(get("/api/properties/{id}", property.getId()))
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.id").value(property.getId()));
+    }
+
+    @Test
+    public void getNonExistingProperty() throws Exception {
+        // Get the property
+        restPropertyMockMvc.perform(get("/api/properties/{id}", Long.MAX_VALUE))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void updateProperty() throws Exception {
+        // Initialize the database
+        propertyRepository.save(property);
+
+        int databaseSizeBeforeUpdate = propertyRepository.findAll().size();
+
+        // Update the property
+        Property updatedProperty = propertyRepository.findById(property.getId()).get();
+
+        restPropertyMockMvc.perform(put("/api/properties")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(updatedProperty)))
+            .andExpect(status().isOk());
+
+        // Validate the Property in the database
+        List<Property> propertyList = propertyRepository.findAll();
+        assertThat(propertyList).hasSize(databaseSizeBeforeUpdate);
+        Property testProperty = propertyList.get(propertyList.size() - 1);
+    }
+
+    @Test
+    public void updateNonExistingProperty() throws Exception {
+        int databaseSizeBeforeUpdate = propertyRepository.findAll().size();
+
+        // Create the Property
+
+        // If the entity doesn't have an ID, it will throw BadRequestAlertException
+        restPropertyMockMvc.perform(put("/api/properties")
+            .contentType(TestUtil.APPLICATION_JSON_UTF8)
+            .content(TestUtil.convertObjectToJsonBytes(property)))
+            .andExpect(status().isBadRequest());
+
+        // Validate the Property in the database
+        List<Property> propertyList = propertyRepository.findAll();
+        assertThat(propertyList).hasSize(databaseSizeBeforeUpdate);
+    }
+
+    @Test
+    public void deleteProperty() throws Exception {
+        // Initialize the database
+        propertyRepository.save(property);
+
+        int databaseSizeBeforeDelete = propertyRepository.findAll().size();
+
+        // Get the property
+        restPropertyMockMvc.perform(delete("/api/properties/{id}", property.getId())
+            .accept(TestUtil.APPLICATION_JSON_UTF8))
+            .andExpect(status().isOk());
+
+        // Validate the database is empty
+        List<Property> propertyList = propertyRepository.findAll();
+        assertThat(propertyList).hasSize(databaseSizeBeforeDelete - 1);
+    }
+
+    @Test
+    public void equalsVerifier() throws Exception {
+        TestUtil.equalsVerifier(Property.class);
+        Property property1 = new Property();
+        property1.setId("id1");
+        Property property2 = new Property();
+        property2.setId(property1.getId());
+        assertThat(property1).isEqualTo(property2);
+        property2.setId("id2");
+        assertThat(property1).isNotEqualTo(property2);
+        property1.setId(null);
+        assertThat(property1).isNotEqualTo(property2);
+    }
+}
